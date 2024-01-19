b90383b41b76b0ef4c013008cb7487781a381a26
Falak Zahra <falak.zahra16@gmail.com>
2024-01-18 23:28:31 -0700

    create and style upload button for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import brainflixLogo from '../../assets/images/BrainFlix-logo.svg';
 import avatar from '../../assets/images/Mohan-muruge.jpg';
+import uploadIcon from '../../assets/images/upload.svg';
 import './Header.scss';
 function Header() {
     return (
@@ -8,8 +9,11 @@ function Header() {
             <form className="header__searchbar">
                 <input className="header__searchbar--input" type="search" placeholder="Search"></input>
             </form>
-            <img className="header__avatar" src={avatar} />
-            <button classname="header__button">UPLOAD</button>
+            <img className="header__avatar" src={avatar} alt="avatar" />
+            <button className="header__button">
+                <img className='button-icon' src={uploadIcon} alt="icon" />
+                <span>UPLOAD</span>
+            </button>
 
         </>
     )
