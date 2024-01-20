1f247890c92a2f1ffe50666267a822515fccbe8a
Falak Zahra <falak.zahra16@gmail.com>
2024-01-19 22:09:25 -0700

    Apply Header Styling for tablet

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <video controls poster="/images/w3html5.gif">
           <source src="movie.mp4" type="video/mp4" />
           <source src="movie.ogg" type="video/ogg" />
-          Your browser does not support the video tag.
         </video>
       </section>
     </>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,15 @@ import './Header.scss';
 function Header() {
     return (
         <>
-            <img className="header__logo" src={brainflixLogo} alt="Brainflix logo" />
-            <form className="header__searchbar">
-                <input className="header__searchbar--input" type="search" placeholder="Search"></input>
+            <header className='header'>
+                <img className="header__logo" src={brainflixLogo} alt="Brainflix logo" />
+                <input className="header__searchbar" type="search" placeholder="Search"></input>
                 <img className="header__avatar" src={avatar} alt="avatar" />
-            </form>
-
-            <button className="header__button">
-                <img className='button-icon' src={uploadIcon} alt="icon" />
-                <span>UPLOAD</span>
-            </button>
-
+                <button className="header__button">
+                    <img className='button-icon' src={uploadIcon} alt="icon" />
+                    <span>UPLOAD</span>
+                </button>
+            </header>
         </>
     )
 }
