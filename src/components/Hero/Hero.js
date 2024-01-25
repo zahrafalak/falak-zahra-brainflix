73929b6c4a51f1656e2c0f1b57e1d61d460a15bf
Falak Zahra <falak.zahra16@gmail.com>
2024-01-24 19:13:50 -0700

    Render main video and comments using React state

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,34 +3,34 @@ import viewsIcon from '../../assets/images/views.svg';
 import likesIcon from '../../assets/images/likes.svg';
 
 const Hero = (props) => {
-    const firstVideo = props.firstVideo;
+    const currentVideo = props.currentVideo;
     return (
         <>
             <div className='hero__banner'>
-                <video className="hero__video" controls poster={firstVideo.image} ></video>
+                <video className="hero__video" controls poster={currentVideo.image} ></video>
             </div>
 
             <section className='hero'>
-                <h1 className='hero__heading'>{firstVideo.title}</h1>
+                <h1 className='hero__heading'>{currentVideo.title}</h1>
                 <hr className='divider--hero-top'></hr>
                 <div className='hero__details'>
                     <div className='hero__details--left'>
-                        <p className='hero__data--channel'>By {firstVideo.channel}</p>
-                        <p className='hero__data'>{formatDate(firstVideo.timestamp)}</p>
+                        <p className='hero__data--channel'>By {currentVideo.channel}</p>
+                        <p className='hero__data'>{formatDate(currentVideo.timestamp)}</p>
                     </div>
                     <div className='hero__details--right'>
                         <div>
                             <img className='hero__icons' src={viewsIcon} alt="views icon" />
-                            <p className='hero__data'>{firstVideo.views}</p>
+                            <p className='hero__data'>{currentVideo.views}</p>
                         </div>
                         <div>
                             <img className='hero__icons' src={likesIcon} alt="" />
-                            <p className='hero__data'>{firstVideo.likes}</p>
+                            <p className='hero__data'>{currentVideo.likes}</p>
                         </div>
                     </div>
                 </div>
                 <hr className='divider'></hr>
-                <p className='hero__description'>{firstVideo.description}</p>
+                <p className='hero__description'>{currentVideo.description}</p>
             </section>
 
         </>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,17 +2,28 @@ import videos from '../../data/video-details.json';
 import Hero from '../Hero/Hero';
 import Comments from '../Comments/Comments';
 import Form from '../Form/Form';
+import { useState } from 'react';
 
 
 
 const Main = () => {
-    const firstVideoObj = videos[0];
-    const firstVideoComments = firstVideoObj.comments;
+
+    const [videoIndex, setVideoIndex] = useState(0)
+    // const firstVideoComments = firstVideoObj.comments;
+    const title = videos[videoIndex].title
+    const commentsArray = videos[videoIndex].comments;
     return (
         <>
-            <Hero firstVideo={firstVideoObj} />
+            <Hero currentVideo={videos[videoIndex]} />
             <Form />
-            <Comments comments={firstVideoComments} />
+            <Comments comments={commentsArray} />
+            {title}
+
+            <div>
+                <button onClick={() => { setVideoIndex(0) }}>First video</button>
+                <button onClick={() => { setVideoIndex(1) }}>Second video</button>
+                <button onClick={() => { setVideoIndex(2) }}>Third video</button>
+            </div>
         </>
     );
 }
