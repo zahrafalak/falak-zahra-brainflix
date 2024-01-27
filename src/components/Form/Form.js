dbfb73be25bf160da5809bbc7bd822b56d3c2439
Falak Zahra <falak.zahra16@gmail.com>
2024-01-27 16:20:10 -0700

    Fix form styling for tablet and desktop

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,26 +4,24 @@ import commentIcon from '../../assets/images/add_comment.svg';
 
 function Form() {
     return (
-        <>
-            <section className='form__section'>
-                <h4 className='form__heading'>3 comments</h4>
-                <div className="form">
-                    <img className="comments__avatar" src={avatarImage} alt="" />
-                    <form className="form__fields">
-                        <div>
-                            <label className="form__label" htmlFor="comment">JOIN THE CONVERSATION</label>
-                            <textarea id="comment" className="form__input--comment" name="text"
-                                placeholder="Add a new comment"></textarea>
-                        </div>
-                        <button className="form__button">
-                            <img className='button-icon' src={commentIcon} alt="icon" />
-                            <span>COMMENT</span>
-                        </button>
-                    </form>
-                </div>
-            </section>
+        <section className='form-section'>
+            <h4 className='form-section__heading'>3 comments</h4>
+            <div className="form">
+                <img className="comments__avatar" src={avatarImage} alt="" />
+                <form className="form__fields">
+                    <div className='form__block'>
+                        <label className="form__label" htmlFor="comment">JOIN THE CONVERSATION</label>
+                        <textarea id="comment" className="form__input" name="text"
+                            placeholder="Add a new comment"></textarea>
+                    </div>
+                    <button className="form__button">
+                        <img className='button-icon' src={commentIcon} alt="icon" />
+                        <span>COMMENT</span>
+                    </button>
+                </form>
+            </div>
             <hr className="divider"></hr>
-        </>
+        </section>
     );
 }
 
