f77e19e21e6369af0e18fd17bc1c9ae760547912
Falak Zahra <falak.zahra16@gmail.com>
2024-01-27 22:27:41 -0700

    Fix side videos styling for desktop

--- src/components/SideVideos/SideVideos.js
+++ src/components/SideVideos/SideVideos.js
@@ -2,10 +2,9 @@ import './SideVideos.scss';
 
 const SideVideos = ({ filteredArray, updateMainVideo }) => {
     return (
-        <>
+        <div className='sidevideo'>
             <h4 className='sidevideo__heading'>NEXT VIDEOS</h4>
             {filteredArray.map((video) => {
-
                 return (
                     <div key={video.id} className='sidevideo__card' onClick={() => { updateMainVideo(video.id); }}>
                         <img className="sidevideo__image" src={video.image} alt="" />
@@ -17,7 +16,7 @@ const SideVideos = ({ filteredArray, updateMainVideo }) => {
 
                 );
             })}
-        </>
+        </div>
     );
 }
 
