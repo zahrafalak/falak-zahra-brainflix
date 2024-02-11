37bcdec2d8f282788a90075d154fb048aa7053fb
Falak Zahra <falak.zahra16@gmail.com>
2024-02-11 08:51:33 -0700

    Fix formatting for Form and Comments component

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,34 +1,38 @@
-import './Comments.scss';
+import "./Comments.scss";
 
 const VideoComments = ({ mainVideoData }) => {
-    const comments = mainVideoData.comments;
-    return (
-        <>
-            {comments.map((comment) => {
-                return (
-                    <div key={comment.id}>
-                        <div className='comments' id={comment.id}>
-                            <img className='comments__avatar' alt="" />
-                            <div className='comments__details'>
-                                <section className='comments__data'>
-                                    <h4 className='comments__data--name'>{comment.name}</h4>
-                                    <p className='comments__data--time'>{formatDate(comment.timestamp)}</p>
-                                </section>
-                                <p className='comments__text'>{comment.comment}</p>
-                            </div>
-                        </div>
-                        <hr className='comments__divider'></hr>
-                    </div>
-                );
-            })}
-        </>
-    );
-}
+  const comments = mainVideoData.comments;
+  return (
+    <>
+      {comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            <div className="comments" id={comment.id}>
+              <img className="comments__avatar" alt="" />
+              <div className="comments__details">
+                <section className="comments__data">
+                  <h4 className="comments__data--name">{comment.name}</h4>
+                  <p className="comments__data--time">
+                    {formatDate(comment.timestamp)}
+                  </p>
+                </section>
+                <p className="comments__text">{comment.comment}</p>
+              </div>
+            </div>
+            <hr className="comments__divider"></hr>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 function formatDate(timestamp) {
-    const commentDate = new Date(timestamp);
-    const formattedDate = `${("0" + (commentDate.getMonth() + 1)).slice(-2)}/${("0" + commentDate.getDate()).slice(-2)}/ ${commentDate.getFullYear()}`;
-    return formattedDate;
+  const commentDate = new Date(timestamp);
+  const formattedDate = `${("0" + (commentDate.getMonth() + 1)).slice(-2)}/${(
+    "0" + commentDate.getDate()
+  ).slice(-2)}/ ${commentDate.getFullYear()}`;
+  return formattedDate;
 }
 
 export default VideoComments;
