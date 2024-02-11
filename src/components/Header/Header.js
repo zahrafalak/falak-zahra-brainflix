7e30c2d0e94c5aaac18cdfcd9f67a9dda349201a
Falak Zahra <falak.zahra16@gmail.com>
2024-02-11 08:49:48 -0700

    Add functionality to Brainflix logo to route to HomePage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,32 @@
-import brainflixLogo from '../../assets/images/BrainFlix-logo.svg';
-import avatar from '../../assets/images/Mohan-muruge.jpg';
-import uploadIcon from '../../assets/images/upload.svg';
-import './Header.scss';
+import brainflixLogo from "../../assets/images/BrainFlix-logo.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import uploadIcon from "../../assets/images/upload.svg";
+import "./Header.scss";
+import { Link } from "react-router-dom";
 const Header = () => {
-    return (
-        <>
-            <header className='header'>
-                <img className="header__logo" src={brainflixLogo} alt="Brainflix logo" />
-                <input className="header__searchbar" type="search" placeholder="Search"></input>
-                <img className="header__avatar" src={avatar} alt="avatar" />
-                <button className="header__button">
-                    <img className='button-icon' src={uploadIcon} alt="icon" />
-                    <span>UPLOAD</span>
-                </button>
-            </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="header">
+        <Link to="/">
+          <img
+            className="header__logo"
+            src={brainflixLogo}
+            alt="Brainflix logo"
+          />
+        </Link>
+        <input
+          className="header__searchbar"
+          type="search"
+          placeholder="Search"
+        ></input>
+        <img className="header__avatar" src={avatar} alt="avatar" />
+        <button className="header__button">
+          <img className="button-icon" src={uploadIcon} alt="icon" />
+          <span>UPLOAD</span>
+        </button>
+      </header>
+    </>
+  );
+};
 
 export default Header;
